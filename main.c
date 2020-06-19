e69bd87ec9862dfdced60eeec35c154f17bdab08
Karina Avalos <avaloskarina04@gmail.com>
2020-06-19 04:26:19 -0700

    add project files

--- main.c
+++ main.c
@@ -42,20 +42,30 @@
 //=  GLOBAL VARIABLES (extern)
 //=    Declared with keyword `extern` in the .h file.
 //=    E.g. int8_t g_error_variable;
+volatile uint16_t SI = 0;         // SI signal to initiate a "line" of pixel
+volatile uint16_t lineTimer;      // Timer signal to keep track of how long a "line" takes
+volatile uint16_t CLK = 0;        // Clock signal that will tell the ADC to convert a value
 uint32_t val;                // Variable that the ADC reads into
 volatile uint32_t ping[128]; // Buffer #1 that will hold a "line" of ADC values
 volatile uint32_t pong[128]; // Buffer #2 that will hold a "line" of ADC values
 volatile uint8_t buffer = 1;     // Buffer pointer that tells the ADC where to store
                              // values: 0 = ping, 1 = pong
-volatile uint16_t index_ping = 0;  // Index that tells the ADC where in ping/pong to store
+volatile uint16_t indexping = 0;  // Index that tells the ADC where in ping/pong to store
 volatile uint16_t index = 0;
-volatile uint16_t index_pong = 0; // the value
-volatile uint16_t clk_count = 0;  // CLK counter that lets the ADC interrupt know when
-            
+volatile uint16_t indexpong = 0; // the value
+volatile uint16_t done = 0;      // Flag that lets the program know when the "line" is done
+volatile uint16_t clkCount = 0;  // CLK counter that lets the ADC interrupt know when
+                            // 128 pixels have been converted
 volatile uint16_t letter;        // Variable that the UART stores into
+volatile bool printFlag = 0; // Flag to print something to UART
+volatile bool copyFlag = 0;  // Similar to above
+volatile bool SysFlag = 0;   //
+volatile bool fullPong = 0;
+volatile bool fullPing = 0;
 volatile uint16_t letter = 0;
 volatile uint16_t max = 0;
 volatile uint16_t min = 0;
+volatile bool b_is_buffer_full = 0;
 volatile uint16_t max_index = 85;
 
 //=======================================================================
@@ -71,22 +81,12 @@ volatile uint16_t max_index = 85;
 //=    Definition of private constants, variables and structures at file scope
 //=    E.g. static uint8_t variable_name;
 //=    E.g. static const uint8_t variable_name_k;
-static bool b_is_buffer_full = 0;
-static bool print_flag = 0; // Flag to print something to UART
-static bool copy_flag = 0;  // Similar to above
-static bool SysFlag = 0;   //
-static bool fullPong = 0;
-static bool fullPing = 0;
-static bool SI = 0;         // SI signal to initiate a "line" of pixel
-static bool line_timer;      // Timer signal to keep track of how long a "line" takes
-static bool CLK = 0;        // Clock signal that will tell the ADC to convert a value
-static bool done = 0;      // Flag that lets the program know when the "line" is done
 
 //=======================================================================
 //=  PRIVATE FUNCTION DECLARATIONS (static)
 //=    Local function prototypes
 //=    E.g. static uint8_t private_function_name(uint8_t param);
-static set_max_index(uint16_t buffer_max_index);
+
 //=======================================================================
 //=  PUBLIC FUNCTION DEFINITIONS
 //=    Definition of the public functions declared first in the .h file
@@ -355,26 +355,36 @@ void UARTIntHandler(void)
 {
     uint32_t ui32Status;
 
+    //
     // Get the interrrupt status.
+    //
     ui32Status = UARTIntStatus(UART0_BASE, true);
 
+    //
     // Clear the asserted interrupts.
+    //
     UARTIntClear(UART0_BASE, ui32Status);
 
 
     while (UARTCharsAvail(UART0_BASE))
     {
-        letter = UARTCharGet(UART0_BASE);
-        UARTCharPutNonBlocking(UART0_BASE, letter);
+        //
+        // Read the next character from the UART and write it back to the UART.
+        //
+        //ROM_UARTCharPutNonBlocking(UART0_BASE,letter);
 
+        letter = UARTCharGet(UART0_BASE);
+        //UARTSend((uint8_t *)"\033[2JEnter text: ", 16);
+        UARTCharPutNonBlocking(UART0_BASE, letter);
+        //ROM_UARTCharPutNonBlocking(UART1_BASE,letter);
 
         if (letter == ('p'))
         {
-            print_flag = 1;
+            printFlag = 1;
         }
         else if (letter == ('c'))
         {
-            copy_flag = 1;
+            copyFlag = 1;
         }
         else if (letter == ('q'))
         {
@@ -384,27 +394,57 @@ void UARTIntHandler(void)
     }
 }
 
-
+///////////////
+//
 // Timer Interrupt Handler, moderated by SysTick
+//
+///////////////
+
 void SysTickHandler(void)
 {
+    //SysFlag = 1;
 
+    //
     // Assert SI = HIGH
+    //
     SI = 1;
     GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6, GPIO_PIN_6);
 
+    //
     // Assert conversion timer = HIGH
-    line_timer = 1;
+    //
+    lineTimer = 1;
     GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_4, GPIO_PIN_4);
 
+    ///////////////
+    // NOTE: SysTick timer interrupt request clears itself, so
+    //       no line of code clearing timer interrupt request
+    //       is included
+    ///////////////
+
+    //
     // Assert CLK signal = HIGH
+    //
     CLK = 1;
     GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_7, GPIO_PIN_7);
 
+    //
     // Begin CLK counter here
-    clk_count = 1;
+    //
+    clkCount = 1;
 
+    //
+    // Reset done flag
+    //
+
+    //
+    // Reset index
+    //
+    // indexping = 0;
+
+    //
     // Change buffer pointer to the opposite, to switch between ping and pong
+    //
     if (b_is_buffer_full)
     {
         if (buffer == 1)
@@ -417,22 +457,45 @@ void SysTickHandler(void)
         }
     }
 
+    //
     // Assert SI = LOW
+    //
     SI = 0;
     GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6, 0);
+
+    //
+    // Start ADC conversion
+    //
     ADCProcessorTrigger(ADC0_BASE, 3);
+
+    //
+    // Assert CLK = LOW
+    //
     CLK = 0;
     GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_7, 0);
-
 }
 
 void ADCHandler(void)
 {
-  
+    //
+    // Assert CLK = HIGH
+    //
     CLK = 1;
     GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_7, GPIO_PIN_7);
+
+    //
+    // Clear ADC interrupt
+    //
+
     ADCIntClear(ADC0_BASE, 3);
+    //
+    // Read ADC value
+    //
     ADCSequenceDataGet(ADC0_BASE, 3, &val);
+
+    //
+    // Store value in either ping or pong, and increment index If buffer is 1 store in pong.
+    //
 
     if (buffer == 0)
     {
@@ -445,78 +508,202 @@ void ADCHandler(void)
         index = index + 1;
     }
 
-    clk_count++;
+    //    if ((fullPong==0) && SI) {
+    //    if (fullPong == 0) {
+    //        pong[indexpong] = val;
+    //        indexpong = indexpong + 1;
+    //    }
+    //    if(indexpong == 128) {
+    //        fullPing = 0;
+    //        fullPong = 1;
+    //        indexpong = 0;
+    //    }
+    //    if(fullPing == 0 && fullPong==1) {
+    //        ping[indexping] = val;
+    //        indexping = indexping + 1;
+    //    }
+    //    if(indexping == 128) {
+    //           fullPong = 0;
+    //           fullPing = 1;
+    //           indexping = 0;
+    //    }
 
-    if (clk_count < 129)
+    //
+    // Increment clock counter
+    //
+    clkCount++;
+
+    //
+    // Check if clock counter < 129
+    // IF < 129: Start next ADC conversion (NOTE: ensure at least 120 ns delay since CLK set high)
+    // ELSE: Assert lineTimer = LOW, Assert done = HIGH
+    //
+
+    //Adjust for middle. Higher max index means turn left 71-99
+    if (clkCount < 129)
     {
         ADCProcessorTrigger(ADC0_BASE, 3);
     }
     else
     {
-       
+        lineTimer = 0;
         GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_4, 0);
-        line_timer = 0;
-        clk_count = 0;
+        clkCount = 0;
         b_is_buffer_full = 1;
         index = 0;
         done = 1;
-
-        if (b_is_buffer_full)
+        if (b_is_buffer_full && buffer)
         {
-            if(buffer)
+            max_index = find_max_index(pong);
+            if ((max_index < 100) && (max_index > 70))
             {
-                max_index = find_max_index(pong);
-                set_max_index(max_index);
-                
+                max_index = 85;
             }
-            else
+        }
+        else if (b_is_buffer_full)
+        {
+            max_index = find_max_index(ping);
+            if ((max_index < 100) && (max_index > 70))
             {
-                max_index = find_max_index(ping);
-                set_max_index(max_index);
+                max_index = 85;
             }
-         
+        }
+        // Left 1 100-109
+        else if (b_is_buffer_full && buffer)
+        {
+            max_index = find_max_index(pong);
+            if ((max_index < 109) && (max_index > 99))
+            {
+                max_index = 105;
+            }
+        }
+        else if (b_is_buffer_full)
+        {
+            max_index = find_max_index(ping);
+            if ((max_index < 109) && (max_index > 99))
+            {
+                max_index = 105;
+            }
+        }
+        // Left 2 110 - 118
+        else if (b_is_buffer_full && buffer)
+        {
+            max_index = find_max_index(pong);
+            if ((max_index < 118) && (max_index > 109))
+            {
+                max_index = 115;
+            }
+        }
+        else if (b_is_buffer_full)
+        {
+            max_index = find_max_index(ping);
+            if ((max_index < 118) && (max_index > 109))
+            {
+                max_index = 115;
+            }
+        }
+        // Left 3 119 - 128
+        else if (b_is_buffer_full && buffer)
+        {
+            max_index = find_max_index(pong);
+            if ((max_index < 129) && (max_index > 118))
+            {
+                max_index = 127;
+            }
+        }
+        else if (b_is_buffer_full)
+        {
+            max_index = find_max_index(ping);
+            if ((max_index < 129) && (max_index > 118))
+            {
+                max_index = 127;
+            }
+        }
+        // Right 1 56-70
+        else if (b_is_buffer_full && buffer)
+        {
+            max_index = find_max_index(pong);
+            if ((max_index < 71) && (max_index > 55))
+            {
+                max_index = 64;
+            }
+        }
+        else if (b_is_buffer_full)
+        {
+            max_index = find_max_index(ping);
+            if ((max_index < 71) && (max_index > 55))
+            {
+                max_index = 64;
+            }
+        }
+        // Right 2 40-55
+        else if (b_is_buffer_full && buffer)
+        {
+            max_index = find_max_index(pong);
+            if ((max_index < 56) && (max_index > 39))
+            {
+                max_index = 48;
+            }
+        }
+        else if (b_is_buffer_full)
+        {
+            max_index = find_max_index(ping);
+            if ((max_index < 56) && (max_index > 39))
+            {
+                max_index = 48;
+            }
+        }
+        // Right 3 20-39
+        else if (b_is_buffer_full && buffer)
+        {
+            max_index = find_max_index(pong);
+            if ((max_index < 40) && (max_index > 19))
+            {
+                max_index = 30;
+            }
+        }
+        else if (b_is_buffer_full)
+        {
+            max_index = find_max_index(ping);
+            if ((max_index < 40) && (max_index > 19))
+            {
+                max_index = 30;
+            }
+        }
+        // Right 4 1-19
+        else if (b_is_buffer_full && buffer)
+        {
+            max_index = find_max_index(pong);
+            if ((max_index < 20) && (max_index > 0))
+            {
+                max_index = 5;
+            }
+        }
+        else if (b_is_buffer_full)
+        {
+            max_index = find_max_index(ping);
+            if ((max_index < 20) && (max_index > 0))
+            {
+                max_index = 5;
+            }
         }
 
+        else
+        {
+        }
         PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0, 1487 - (max_index * 5));
+        //        if(fullPing) {
+        //            fullPing =0;
+        //
+        //        }
+        //        else {
+        //            fullPong =0;
+        //        }
     }
-    
+
+    //
+    // Set CLK = LOW
+    //
     CLK = 0;
     GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_7, 0);
-}
-
-// Adjusts the max index to set values based on range it is in 
-static set_max_index(uint16_t buffer_max_index)
-{
-    if ((buffer_max_index < 109) && (buffer_max_index > 99))
-    {
-        max_index = 105;
-    }
-    if ((buffer_max_index < 118) && (buffer_max_index > 109))
-    {
-        max_index = 115;
-    }
-    if ((buffer_max_index < 71) && (buffer_max_index > 55))
-    {
-        max_index = 64;
-    }
-    if ((buffer_max_index < 56) && (buffer_max_index > 39))
-    {
-        max_index = 48;
-    }
-    if ((buffer_max_index < 40) && (buffer_max_index > 19))
-    {
-        max_index = 30;
-    }
-    if ((buffer_max_index < 20) && (buffer_max_index > 0))
-    {
-        max_index = 5;
-    }
-    if ((buffer_max_index < 129) && (buffer_max_index > 118))
-    {
-        max_index = 127;
-    }
-    if ((buffer_max_index < 100) && (buffer_max_index > 70))
-    {
-        max_index = 85;
-    }
 }
